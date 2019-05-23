e37408b4e47ddbcd724bb60cbfeb5a419e3a4e81
lukaszogan <lukasz.ogan@gmail.com>
2019-05-23 16:55:58 +0200

    add another example

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -2,6 +2,10 @@ package com.sda.algorytmy;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -18,6 +22,30 @@ public class App
         int pierwszy = tab[0];
         tab = Arrays.copyOfRange(tab, 1, tab.length);
         return pierwszy + suma(tab);
+    }
+
+    private static List<Integer> quicksortExample2(List<Integer> list) {
+        if (list.size() < 2) {
+            // base case, arrays with 0 or 1 element are already "sorted"
+            return list;
+        } else {
+            // recursive case
+            Integer pivot = list.get(0);
+
+            // sub-array of all the elements less than the pivot
+            List<Integer> less = list.stream().skip(1).filter(el -> el <= pivot)
+                    .collect(Collectors.toList());
+
+            // sub-array of all the elements greater than the pivot
+            List<Integer> greater = list.stream().skip(1).filter(el -> el > pivot)
+                    .collect(Collectors.toList());
+
+            return Stream.of(
+                    quicksortExample2(less).stream(),
+                    Stream.of(pivot),
+                    quicksortExample2(greater).stream())
+                    .flatMap(Function.identity()).collect(Collectors.toList());
+        }
     }
 
     public static void quicksort(int[] tab, int x, int y){
